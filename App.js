4f7469f363a911aa17f4463ab32dcd1c37c7b35d
nabeelCode <mail.nabeelmuhammed@gmail.com>
2020-11-24 08:46:12 +0530

    Temperature Calculator[2] - Logic Optimised

--- App.js
+++ App.js
@@ -5,27 +5,34 @@ import {Style} from './styles/Style'
 export default App = () => {
     const [c,setC] = useState('Enter in Celsius')
     const [f,setF] = useState('Enter in Fahrenheit')
-    var [boilStatus,setBoilStatus]=useState('Water will not Boil')
+    const [boilStatus,setBoilStatus]=useState('Water will not Boil')
+    
 
-    const fChangeText = (temp) => {
-        temp >= 100 ? setBoilStatus('Water will Boil') : setBoilStatus('Water will not Boil')
-
+    const changeText = (temp,mode) => {
+        var celsiusToBoil = 0
+        var temperature = 0
+        /**
+         *  parameters : temp - onChangeText data, mode - to check whether the temperature is given
+            in celsius field or fahrenheit field.
+         *  local data celsiusToBoil includes celsius data to check whether water will boil or not
+         *  local data temperature in celsius mode will calculate correponding fahrenheit and celsius 
+         *  in fahrenheit mode will calculate corresponsing celsius data.
+         */
         if(temp == '')
             return 'Enter in Fahrenheit'
         else{
-            return ((temp * 9 / 5) + 32).toString()
+            if(mode == 'celsius'){
+                celsiusToBoil = temp
+                temperature = ((temp * 9 / 5) + 32).toString()
+            }
+            else if(mode == 'fahrenheit'){
+                celsiusToBoil = (temp - 32) * 5 / 9
+                temperature = celsiusToBoil.toString() 
+            }
+            celsiusToBoil >= 100 ? setBoilStatus('Water will Boil') : setBoilStatus('Water will not Boil')
+            return temperature
         }
     }
-
-    const cChangeText = (temp) => {
-        const celsius = (temp - 32) * 5 / 9
-        celsius >= 100 ? setBoilStatus('Water will Boil') : setBoilStatus('Water will not Boil')
-        if(temp == '')
-            return 'Enter in Celcius'
-        else{
-            return (celsius.toString())
-        }
-    }  
 
 
 
@@ -36,7 +43,7 @@ export default App = () => {
                     style={Style.inputStyle} 
                     placeholder={c.toString()}
                     onChangeText={(temp)=>{
-                        setF(fChangeText(temp))
+                        setF(changeText(temp,"celsius"))
                     }}
                 />
                 <View style={Style.line}/>
@@ -46,7 +53,7 @@ export default App = () => {
                     style={Style.inputStyle} 
                     placeholder={f.toString()}
                     onChangeText={(temp)=>{
-                        setC(cChangeText(temp))
+                        setC(changeText(temp,"fahrenheit"))
                     }}
                 />
                 <View style={Style.line}/>
